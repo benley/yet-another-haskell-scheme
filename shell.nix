c14ab29fb7dfe0668786f8b0cb0792c53d740d8a
Benjamin Staffin <bstaffin@fitbit.com>
2016-03-13 01:07:28 -0500

    What I've got so far (why didn't I start this in git?)

---
+++ shell.nix
@@ -0,0 +1,19 @@
+{ nixpkgs ? import <nixpkgs> {}
+, compiler ? "ghc7103"
+}:
+
+with nixpkgs;
+
+let ghc = haskell.packages.${compiler}.ghcWithPackages (x: with x; [
+  ghc-mod
+  parsec
+  hdevtools
+  hlint
+  hoogle
+]);
+
+in stdenv.mkDerivation rec {
+  name = "haskellDev";
+  buildInputs = [haskellPackages.ghc];
+  shellHook = "eval $(grep ^export ${ghc}/bin/ghc)";
+}
